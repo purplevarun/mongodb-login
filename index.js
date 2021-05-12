d103d6b9df85d3ca56c13826cef8daa15e7d1896
purplevarun <purplevarun@gmail.com>
2021-05-12 21:27:12 +0530

    bsdk

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const InitiateMongoServer = require("./config/db");
+
+// Initiate Mongo Server
+InitiateMongoServer();
 
 const app = express();
 
 // PORT
 const PORT = process.env.PORT || 4000;
+
+// Middleware
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.json({ message: "API Working" });
