d570638adc959a967fa098e7c21a316757ecb90b
purplevarun <purplevarun@gmail.com>
2021-05-12 17:47:40 +0530

    mongo

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ const express = require('express')
 const app = express();
 const mongodb = require('mongodb');
 const port = process.env.PORT || 3000;
+const MongoClient = mongodb.MongoClient;
+const url = 'mongodb://localhost';
+const dbname = 'db1';
+const dataInsert = (name,pass,email) => {
+	MongoClient.connect(
+		url,
+		{useNewUrlParser:true,useUnifiedTopology:true},
+	 	(err,client) =>{
+		if (err)
+			return console.log(err);
+		console.log('mongo connected');
+		const db = client.db(dbname);
+		var data = {
+			'name' : name,
+			'emailID' : email,
+			'password' : pass 
+		};
+		db.collection ("test").insertOne(data);
+	});
+};
 app.set ('view engine','ejs');
 app.use (express.static('static'));
 app.get('/',(req,res) => {
@@ -16,6 +36,9 @@ app.get ('/login', (req,res) => {
 app.get('/register', (req,res) => {
 	res.render('register');
 })
+app.post('/register', (req,res) => {
+
+});
 app.listen(port,() => {
 	console.log('server started on ',port);
 });
