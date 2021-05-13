4287d181a70c4e720fdd189d02da2c286186c3bd
purplevarun <purplevarun@gmail.com>
2021-05-13 10:08:59 +0530

    nayi-bahaar

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require ('express');
+const app = express ();
+const mongoose = require ('mongoose');
+const passport = require ('body-parser');
+const LocalStrategy = require ('passport-local');
+const passportMongoose = require('passport-local-mongoose');
+const User = require ('./models/User');
+
+mongoose.set('useNewUrlParser',true);
+mongoose.set('useFindAndModify',true);
+mongoose.set('useCreateIndex',true);
+mongoose.set('useUnifiedTopology',true);
+
+const mongourl = 'mongodb://localhost/db1';
+
+mongoose.connect (mongourl);
