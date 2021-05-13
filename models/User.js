9e22ce80ccb5906f2a367d2cab102cc09628fbd5
purplevarun <purplevarun@gmail.com>
2021-05-13 10:50:28 +0530

    mkc

--- models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-var UserSchema = new mongoose.Schema({
-    username : String,
-    email : String,
-    password : String
-}, {timestamps: true});
-
-module.exports = mongoose.model('User', UserSchema);
