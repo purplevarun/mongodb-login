ecc72173cfc2fb2ad32ed7a0a9a8def9f351e542
purplevarun <purplevarun@gmail.com>
2021-05-13 16:21:08 +0530

    last-chance

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require ('express');
 const app = express();
 const bodyparser = require('body-parser');
 const mongoose = require('mongoose');
-
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const mongoURL = 'mongodb://localhost/login-promject';
+const port = process.env.PORT || 3000;
+
 mongoose.connect (mongoURL,{
 	useNewUrlParser: true,
 	useUnifiedTopology: true
@@ -11,4 +14,26 @@ mongoose.connect (mongoURL,{
 	if (err)
 		console.log('error = ',err);
 	else console.log('mongo connected');
+});
+
+const db = mongoose.connection;
+db.on ('error', console.error.bind(console,'connection error:'));
+db.once ('open', () => {});
+
+app.use (session ({
+	secret : 'purplevarun',
+	resave : true,
+	saveUninitialized: false,
+	store : MongoStore.create({
+		mongooseConnection : db
+	})
+}));
+
+app.set ('view engine', 'ejs');
+app.use (bodyparser.json());
+app.use (bodyparser.urlencoded({extended:false}));
+
+
+app.listen(port, () => {
+	console.log('server running on ',port);
 });
